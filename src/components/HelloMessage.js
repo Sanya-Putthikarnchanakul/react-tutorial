5c18b1593aa6bc77537115229fdacbf6d034dc0d
Sanya P <sanya_ped@yahoo.com>
2021-09-12 02:01:45 +0700

    React Home Page Code Example

---
+++ src/components/HelloMessage.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class HelloMessage extends Component {
+    render() {
+        return (
+            <div>
+                Hello {this.props.name}
+            </div>
+        );
+    }
+}
+
+export default HelloMessage;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//import App from './App';
+//import HelloMessage from './components/HelloMessage';
+//import Timer from './components/Timer';
+//import TodoApp from './components/TodoApp';
+import MarkdownEditor from './components/MarkdownEditor';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<MarkdownEditor/>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
